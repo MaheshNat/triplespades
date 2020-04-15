619f0a84f666c24d9ea311aae4283585085e0c60
Arjun Vikram <arjvik@users.noreply.github.com>
2020-04-14 21:06:46 -0500

    Make your code more pythonic
    :thinking:

--- tutorials/basics/solutions/ttt.py
+++ tutorials/basics/solutions/ttt.py
@@ -18,7 +18,6 @@ def print_board():
 def check(a, b, c):
     return a == b == c and not a == '-'
 
-
 def check_for_win():
     # checking rows
     for row in board:
@@ -34,13 +33,23 @@ def check_for_win():
     if check(board[2][0], board[1][1], board[0][2]):
         return board[2][0]
 
+def check_for_win_better():
+    # checking rows
+    for winner in [row[0] for row in b if check(*row)]:
+        return winner
+    # checking columns
+    for winner in [row[0] for row in list(zip(*b)) if check(*row)]: 
+        return winner
+    # checking diagonals
+    # this last one is just a meme, nobody can understand it
+    for winner in [b[::t][0][0] for t in [1,-1] if check(*[b[::t][i][i] for i in range(3)])]:
+        return winner
+
 
 while not game_ended:
     while not round_ended:
-        row = int(input(
-            f'Player {turn}, enter a row to place your marker [0, 2]: '))
-        col = int(input(
-            f'Player {turn}, enter a column to place your marker [0, 2]: '))
+        row = int(input(f'Player {turn}, enter a row to place your marker [0, 2]: '))
+        col = int(input(f'Player {turn}, enter a column to place your marker [0, 2]: '))
         if board[row][col] != '-':
             print('You cannot place a marker at that location!')
             continue
@@ -48,7 +57,7 @@ while not game_ended:
         print_board()
         win = check_for_win()
         turn = 'O' if turn == 'X' else 'X'
-        if(win != None):
+        if (win):
             print(f'Congratulations, player {win} won the game!')
             round_ended = True
     play_again = input('Would you like to play again? (y / n): ')
